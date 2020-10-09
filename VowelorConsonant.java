87dd54db268489a7cf554221f5e99339cc7d971a
Bronson <58447783+BronsonN@users.noreply.github.com>
2020-10-09 17:05:32 -0600

    Add files via upload

---
+++ VowelorConsonant.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+
+public class VowelorConsonant {
+
+  public static void main(String [] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Enter a letter: ");
+    String let = keyboard.nextLine();
+    char letter = let.charAt(0);
+
+    switch (letter) {
+      case 'a':
+          System.out.println((letter) + " is a vowel");break;
+      case 'A':
+          System.out.println((letter) + " is a vowel");break;
+      case 'e':
+          System.out.println((letter) + " is a vowel");break;
+      case 'E':
+          System.out.println((letter) + " is a vowel");break;
+      case 'i':
+          System.out.println((letter) + " is a vowel");break;
+      case 'I':
+          System.out.println((letter) + " is a vowel");break;
+      case 'o':
+          System.out.println((letter) + " is a vowel");break;
+      case 'O':
+          System.out.println((letter) + " is a vowel");break;
+      case 'u':
+          System.out.println((letter) + " is a vowel");break;
+      case 'U':
+          System.out.println((letter) + " is a vowel");break;
+      case 'b':
+          System.out.println((letter) + " is a consonant");break;
+      case 'B':
+          System.out.println((letter) + " is a consonant");break;
+      case 'c':
+          System.out.println((letter) + " is a consonant");break;
+      case 'C':
+          System.out.println((letter) + " is a consonant");break;
+      case 'd':
+          System.out.println((letter) + " is a consonant");break;
+      case 'D':
+          System.out.println((letter) + " is a consonant");break;
+      case 'f':
+          System.out.println((letter) + " is a consonant");break;
+      case 'F':
+          System.out.println((letter) + " is a consonant");break;
+      case 'g':
+          System.out.println((letter) + " is a consonant");break;
+      case 'G':
+          System.out.println((letter) + " is a consonant");break;
+      case 'h':
+          System.out.println((letter) + " is a consonant");break;
+      case 'H':
+         System.out.println((letter) + " is a consonant");break;
+      case 'j':
+          System.out.println((letter) + " is a consonant");break;
+      case 'J':
+          System.out.println((letter) + " is a consonant");break;
+      case 'k':
+          System.out.println((letter) + " is a consonant");break;
+      case 'K':
+          System.out.println((letter) + " is a consonant");break;
+      case 'l':
+          System.out.println((letter) + " is a consonant");break;
+      case 'L':
+          System.out.println((letter) + " is a consonant");break;
+      case 'm':
+          System.out.println((letter) + " is a consonant");break;
+      case 'M':
+          System.out.println((letter) + " is a consonant");break;
+      case 'n':
+          System.out.println((letter) + " is a consonant");break;
+      case 'N':
+          System.out.println((letter) + " is a consonant");break;
+      case 'p':
+          System.out.println((letter) + " is a consonant");break;
+      case 'P':
+          System.out.println((letter) + " is a consonant");break;
+      case 'q':
+          System.out.println((letter) + " is a consonant");break;
+      case 'Q':
+          System.out.println((letter) + " is a consonant");break;
+      case 'r':
+          System.out.println((letter) + " is a consonant");break;
+      case 'R':
+          System.out.println((letter) + " is a consonant");break;
+      case 's':
+          System.out.println((letter) + " is a consonant");break;
+      case 'S':
+          System.out.println((letter) + " is a consonant");break;
+      case 't':
+          System.out.println((letter) + " is a consonant");break;
+      case 'T':
+          System.out.println((letter) + " is a consonant");break;
+      case 'v':
+          System.out.println((letter) + " is a consonant");break;
+      case 'V':
+          System.out.println((letter) + " is a consonant");break;
+      case 'w':
+          System.out.println((letter) + " is a consonant");break;
+      case 'W':
+          System.out.println((letter) + " is a consonant");break;
+      case 'x':
+          System.out.println((letter) + " is a consonant");break;
+      case 'X':
+          System.out.println((letter) + " is a consonant");break;
+      case 'z':
+          System.out.println((letter) + " is a consonant");break;
+      case 'Z':
+          System.out.println((letter) + " is a consonant");break;
+      case 'y':
+          System.out.println((letter) + " is a consonant");break;
+      case 'Y':
+          System.out.println((letter) + " is a consonant");break;
+      default:
+          System.out.println((letter) + " is an invalid input");
+}
+  }
+
+
+}
